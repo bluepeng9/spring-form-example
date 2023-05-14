9fe946b46a1ed1e9d8301f6d0aba03bcb5406a8f
bluepeng9 <bluepeng9@gmail.com>
2023-05-14 16:13:17 +0900

    feat: Add new fields and classes to the item domain

---
+++ src/main/java/hello/itemservice/domain/item/itemType.java
@@ -0,0 +1,12 @@
+package hello.itemservice.domain.item;
+
+public enum itemType {
+
+    BOOK("도서"), FOOD("음식"), ETC("기타");
+
+    private final String description;
+
+    itemType(String description) {
+        this.description = description;
+    }
+}
