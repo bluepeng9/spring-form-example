e1a1e386f71a2b5df02ed1d6eb88f10565e9b2a1
bluepeng9 <bluepeng9@gmail.com>
2023-05-14 18:41:51 +0900

    refactor: Change itemType enum to follow naming convention

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -1,12 +1,12 @@
 package hello.itemservice.domain.item;
 
-public enum itemType {
+public enum ItemType {
 
     BOOK("도서"), FOOD("음식"), ETC("기타");
 
     private final String description;
 
-    itemType(String description) {
+    ItemType(String description) {
         this.description = description;
     }
 }
